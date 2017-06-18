54260c38586c29ed80916728aa0e4abed20cdcc5
Tomelin <rafael.tomelin@gmail.com>
2017-06-18 19:07:13 -0300

    Change confidir

--- manifests/resource/configdir.pp
+++ manifests/resource/configdir.pp
@@ -1,25 +1,21 @@
 define mailscanner::resource::configdir (
 
-  Enum['present','absent']  $ensure = 'present',
-  $source   = undef,
-  $content  = undef,
+  Enum['directory','absent']  $ensure = 'directory',
   $path,
   $mode     = '0644',
+  $owner    = 'root',
+  $group    = 'root',
   ){
 
     if $source and $content {
       fail 'You must provide either \'source\' or \'content\', not both'
     }
 
-    if $ensure == 'present' {
-	$ensure = 'directory',
-    }
-
     file { "Create directory ${title} for MailScanner":
       ensure  => $ensure,
       path    => $path,
       mode    => $mode,
-      owner   => 'root',
-      group   => 'root',
+      owner   => $owner,
+      group   => $group,
     }
 }
